b68802b51fe69faa6ab80c133c560ad706fc7b78
vandemjh <jack@jackv.dev>
2025-03-14 11:40:24 -0400

    fix

--- src/routes/rss.js
+++ src/routes/rss.js
@@ -22,7 +22,7 @@ router.get('/', (req, res) => {
     ],
   });
 
-  const getUrl = (uuid, extension, audioFolder = './audio') =>
+  const getUrl = (uuid, extension, audioFolder = './data/audio') =>
     new URL(`${getFullURL()}${audioFolder}/${uuid}.${extension}`).href;
 
   feedService.getFeed().forEach((item) =>
